86c01cab8f05617c13f7cc0d309c8c5c475549ec
Dimitris Yfantidis <ydimitri@csd.auth.gr>
2022-08-17 01:31:27 +0300

    improvements (epic)

--- main.cpp
+++ main.cpp
@@ -13,20 +13,45 @@ private:
     std::chrono::time_point<std::chrono::_V2::system_clock> t1;
 
 public:
-    explicit timer(std::ostream& arg) : os(arg) {
-        t0 = std::chrono::high_resolution_clock::now();
-    }
+    explicit timer(std::ostream& arg)
+    :   os(arg),
+        t0(std::chrono::high_resolution_clock::now())
+    {}
 
-    ~timer() {
+    ~timer()
+    {
         t1 = std::chrono::high_resolution_clock::now();
-        os << "\nFunction time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << "ms\n";
+        os  << "\nFunction time: "
+            << static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()) / 1000.0
+            << "ms\n";
     }
 };
 
 int main()
 {
+    int n;
+
+    std::cout << "Give number: ";
+    std::cin >> n;
+
+    std::cout << '\n';
+
     timer t(std::cout);
 
+    BigInteger fib_n  = BigInteger::fibonacci(n);
+    BigInteger fact_n = BigInteger::factorial(n);
+    BigInteger cat_n  = BigInteger::catalan(n);
+
+    BigInteger u1 = fib_n ^ 3;
+    BigInteger u2 = fact_n % cat_n;
+
+    std::cout << "\nVAL: fibonacci(" << n << ") = " << fib_n << '\n'
+              << "\nVAL: " << n << "! = " << fact_n << '\n'
+              << "\nVAL: catalan(" << n << ") = " << cat_n << '\n';
+
+    std::cout << "\nEXPR: fibonacci(" << n << ")^2 = " << u1 << '\n'
+              << "\nEXPR: " << n << "! % catalan(" << n << ") = " << u2 << '\n'
+              << std::endl;
 
     return 0;
 }
